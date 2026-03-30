ce3cf93b54ea9b1bc787ba5ca4eaa17fc07cc0c2
Gaganesh Sharma <ggnsharma007@gmail.com>
2026-03-30 15:15:06 +0530

    Some collections added

--- components/CollectionGrid.tsx
+++ components/CollectionGrid.tsx
@@ -1,10 +1,28 @@
 const collections = [
-  "Snow Speckle",
-  "Grey Shine",
-  "Sprinkle Glow",
-  "Plain Matte",
-  "50-50 Blend",
-  "White Glow",
+  {
+    name: "Snow Speckle",
+    image: "/collections/snow.png",
+  },
+  {
+    name: "Grey Shine",
+    image: "/collections/grey.png",
+  },
+  {
+    name: "Sprinkle Glow",
+    image: "/collections/sprinkle.png",
+  },
+  {
+    name: "Plain Matte",
+    image: "/collections/matte.png",
+  },
+  {
+    name: "50-50 Blend",
+    image: "/collections/blend.png",
+  },
+  {
+    name: "White Glow",
+    image: "/collections/white.png",
+  },
 ];
 
 export default function CollectionGrid() {
@@ -14,11 +32,14 @@ export default function CollectionGrid() {
 
       <div className="grid grid-cols-3 gap-6">
         {collections.map((item, index) => (
-          <div
-            key={index}
-            className="h-60 bg-gray-200 rounded-xl flex items-end p-4 hover:scale-105 transition"
-          >
-            <h3 className="text-lg font-semibold">{item}</h3>
+          <div key={index} className="relative rounded-xl overflow-hidden group">
+            <img
+              src={item.image}
+              className="w-full h-60 object-cover group-hover:scale-110 transition"
+            />
+            <div className="absolute bottom-0 bg-black/50 w-full p-3 text-white">
+              {item.name}
+            </div>
           </div>
         ))}
       </div>
