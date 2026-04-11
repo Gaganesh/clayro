eb5fb2820a3c2c66d79764f8951609b229b72b8c
Gaganesh Sharma <ggnsharma007@gmail.com>
2026-04-11 11:34:56 +0530

    Loader added.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,10 @@
 import ProductsClient from "@/components/ProductList";
 import { Suspense } from "react";
+import LoadingProducts from "./loading";
 
 export default function ProductsPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingProducts/>}>
       <ProductsClient />
     </Suspense>
   );
